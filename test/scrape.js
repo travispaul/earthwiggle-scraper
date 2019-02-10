011c261aa29ff56eba434fdecfdec322aa4c713b
Travis Paul <tr@vispaul.me>
2019-02-10 20:01:28 +0800

    Finish tests for db code.

--- test/scrape.js
+++ test/scrape.js
@@ -11,6 +11,8 @@ const
     parseBody,
     checkSchema,
     loadSchema,
+    insertRecords,
+    saveETag,
     db
   } = require('../lib/scrape'),
   config = require('../lib/config');
@@ -125,6 +127,94 @@ describe('Scrape data', () => {
         }
         checkSchema({}, (error, context) => {
           assert(!context.createTables, 'loadSchema did not create schema');
+          done();
+        });
+      });
+    });
+  });
+
+  describe('insertRecords', () => {
+    const records = [
+      { link: 'https://earthquake.phivolcs.dost.gov.ph/2019_Earthquake_Information/February/2019_0210_0321_B1.html',
+        img: 'https://earthquake.phivolcs.dost.gov.ph/2019_Earthquake_Information/February/2019_0210_0321_B1.jpg',
+        event: '10 February 2019 - 11:21 AM',
+        latitude: '07.58',
+        longitude: '126.74',
+        depth: '045',
+        magnitude: '2.8',
+        location: '020 km N 90° E of Baganga (Davao Oriental)'
+      },
+      {
+        link: 'https://earthquake.phivolcs.dost.gov.ph/2019_Earthquake_Information/February/2019_0210_0306_B1F.html',
+        img: 'https://earthquake.phivolcs.dost.gov.ph/2019_Earthquake_Information/February/2019_0210_0306_B1F.jpg',
+        event: '10 February 2019 - 11:06 AM',
+        latitude: '09.86',
+        longitude: '126.57',
+        depth: '006',
+        magnitude: '4.5',
+        location: '047 km N 80° E of General Luna (Surigao Del Norte)'
+      },
+      {
+        link: 'https://earthquake.phivolcs.dost.gov.ph/2019_Earthquake_Information/February/2019_0210_0304_B1.html',
+        img: 'https://earthquake.phivolcs.dost.gov.ph/2019_Earthquake_Information/February/2019_0210_0304_B1.jpg',
+        event: '10 February 2019 - 11:04 AM',
+        latitude: '09.86',
+        longitude: '126.54',
+        depth: '012',
+        magnitude: '2.9',
+        location: '043 km N 78° E of General Luna (Surigao Del Norte)'
+      }
+    ];
+
+    it('Insert new records', done => {
+      insertRecords({records: records}, (error) => {
+        if (error) {
+          console.error(error);
+          assert(!error, 'Error while inserting records');
+        }
+        db.select('*').from(config.type).then(rows => {
+          assert(rows.length === 3);
+          done();
+        });
+      });
+    });
+
+    it('Do not insert existing records', done => {
+      insertRecords({records: records}, (error) => {
+        if (error) {
+          console.error(error);
+          assert(!error, 'Error while inserting records');
+        }
+        db.select('*').from(config.type).then(rows => {
+          assert(rows.length === 3);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('saveETag', () => {
+    it('Insert a new ETag', done => {
+      saveETag({etag: '123'}, (error) => {
+        if (error) {
+          console.error(error);
+          assert(!error, 'Error while inserting ETag');
+        }
+        db.select('*').from('etag').where({etag: '123'}).then(rows => {
+          assert(rows.length === 1);
+          done();
+        });
+      });
+    });
+
+    it('Do not reinsert ETag', done => {
+      saveETag({etag: '123'}, (error) => {
+        if (error) {
+          console.error(error);
+          assert(!error, 'Error while inserting ETag');
+        }
+        db.select('*').from('etag').where({etag: '123'}).then(rows => {
+          assert(rows.length === 1);
           db.destroy();
           done();
         });
